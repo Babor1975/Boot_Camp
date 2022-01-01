3cc5110f244b35b0a35ae8a4180ae01fdab3a29c
Babor1975 <baborkhademi@gmail.com>
2022-01-01 12:40:31 -0500

    new

---
+++ src/main/java/Boot_Camp/App.java
@@ -0,0 +1,13 @@
+package Boot_Camp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
